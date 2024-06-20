8861d839e39785a4d74e08bb9ea42c101c0235ee
pbogonko <polycarpbogonko1@gmail.com>
2024-06-21 00:34:45 +0300

    login page test

---
+++ login/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import LoginForm from './components/login'
+
+function App() {
+  
+
+  return (
+    <>
+      <LoginForm/>
+    </>
+  )
+}
+
+export default App
---
+++ login/src/components/loginComponent.js
@@ -0,0 +1,12 @@
+const login = (email) => {
+    
+  
+    if (email === 'polycarpbogonko1@gmail.com') {
+return true
+  }else{
+    return false
+  }
+}
+  
+  export default login;
+  
